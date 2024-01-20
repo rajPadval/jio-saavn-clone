c741a5af6ad452c94b7a0bf0920a253545db9623
rajPadval <rajpadval145@gmail.com>
2024-01-20 11:10:47 +0530

    fixed player styling

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import { FaPlay, FaPause } from "react-icons/fa";
+import { IoMdSkipForward, IoMdSkipBackward } from "react-icons/io";
+import { BiRepeat } from "react-icons/bi";
+import { PiShuffleBold } from "react-icons/pi";
+import { HiSpeakerWave } from "react-icons/hi2";
 const Player = () => {
   const [timing, setTiming] = useState(0);
 
@@ -8,7 +12,7 @@ const Player = () => {
   };
 
   return (
-    <div className="fixed bottom-0 right-0 left-0 bg-[#f5f5f5ff]">
+    <div className="fixed bottom-0 right-0 left-0 bg-[#f5f5f5ff] flex flex-col ">
       <input
         type="range"
         min="0"
@@ -18,15 +22,28 @@ const Player = () => {
         onChange={handleProgressChange}
         className="w-full h-[5px] text-green-400 range"
       />
-      <div>
-        <img
-          src="https://c.saavncdn.com/903/Tu-hai-kahan-Hindi-2024-20240111181431-500x500.jpg"
-          alt=""
-          width={70}
-        />
+      <div className="flex justify-between items-center mb-3 px-3">
+        <div>
+          <img
+            src="https://c.saavncdn.com/903/Tu-hai-kahan-Hindi-2024-20240111181431-500x500.jpg"
+            alt=""
+            width={55}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="flex text-3xl gap-6">
+          <BiRepeat className="text-gray-400 " />
+          <IoMdSkipBackward className="text-gray-700 hover:text-gray-500 cursor-pointer" />
+          <FaPlay className="text-gray-700 hover:text-gray-500 cursor-pointer" />
+          <IoMdSkipForward className="text-gray-700 hover:text-gray-500 cursor-pointer" />
+          <PiShuffleBold className="text-gray-400  cursor-pointer" />
+
+        </div>
+        <div>
+          <HiSpeakerWave className="text-gray-700 hover:text-gray-500 text-3xl cursor-pointer" />
+        </div>
       </div>
-      <div></div>
-      <div></div>
+
     </div>
   );
 };
